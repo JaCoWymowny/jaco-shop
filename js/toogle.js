4eb287dc3571ef4c5c2c64c9e49986cd16832205
JaCoWymowny <jacekkombo@gmail.com>
2021-08-02 19:02:26 +0200

    First part of making calc. html is done, not responsive.

---
+++ js/toogle.js
@@ -0,0 +1,33 @@
+const selectMenu = document.querySelector('.form__options select');
+const html = document.querySelector('html');
+
+function changeArrow() {
+  if (selectMenu.className === 'closed__image') {
+    selectMenu.classList.remove('.closed__image');
+    selectMenu.className = 'opened__image';
+  }
+  else {
+    selectMenu.classList.remove('.opened__image');
+    selectMenu.className = 'closed__image';
+  }
+}
+
+html.addEventListener('click', function(e) {
+  if (e.target !== selectMenu) {
+    selectMenu.className = 'closed__image';
+  }
+})
+
+selectMenu.addEventListener('click', changeArrow);
+
+
+// selectMenu.addEventListener('click', function() {
+//   if (selectMenu.style.backgroundImage === )
+//   selectMenu.style.backgroundImage = 'url("../images/arrow2.png")';
+// })
+//
+
+
+//if (selectMenu.style.backgroundImage === "url(\"images/arrow.png\")")
+
+
