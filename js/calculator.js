4bec0580deec0dcd40fbb86af6638440cdec857c
JaCoWymowny <jacekkombo@gmail.com>
2021-08-04 13:15:13 +0200

    Chyba all

--- js/calculator.js
+++ js/calculator.js
@@ -4,7 +4,6 @@ const packageLine = document.querySelector('.summary_package');
 const accountingLine = document.querySelector('.summary_accounting');
 const terminalLine = document.querySelector('.summary_terminal');
 const totalSum = document.querySelector('.total_sum');
-const allOfCostLine = document.querySelectorAll('.summary_calc');
 const firstNumberInput = document.querySelector('.numbers_first-field');
 const secondNumberInput = document.querySelector('.numbers-second-field');
 const packageListChoiceText = document.querySelector('#dropdown_select');
@@ -12,6 +11,7 @@ const packageFullList = document.querySelectorAll('.dropdown li');
 const firstCheckbox = document.querySelector('#first-value');
 const secondCheckbox = document.querySelector('#second-value');
 const formCheckbox = document.querySelectorAll('.checkboxs');
+const allInputs = document.querySelectorAll('input[type="number"]');
 
 function updateCost() {
   let sum = 0;
@@ -23,6 +23,14 @@ function updateCost() {
   packageFullList.forEach((el) => {
     if (el.textContent === packageListChoiceText.textContent) {
       sum += parseFloat(el.value);
+    }
+  })
+  allInputs.forEach((el, index) => {
+    if (index === 1 && el.value > 0) {
+      sum += parseFloat(el.value) * 2;
+    }
+    if (index === 0 && el.value > 0) {
+      sum += parseFloat(el.value) * 5;
     }
   })
   totalSum.innerHTML = sum;
@@ -37,19 +45,19 @@ function showProductCost() {
     productLine.children[1].textContent = +firstNumberInput.value + ' * 5 $';
     sum = +firstNumberInput.value * 5;
   }
-  productLine.children[2].textContent = sum + ' $';
+  productLine.children[2].children[0].textContent = sum;
 }
 
 function showOrderCost() {
   let sum = 0;
-  if (secondNumberInput.value <= 0) {
+  if (+secondNumberInput.value <= 0) {
     ordersLine.style.display = 'none';
   } else {
     ordersLine.style.display = 'flex';
     ordersLine.children[1].textContent = secondNumberInput.value + ' * 2 $';
     sum = +secondNumberInput.value * 2;
   }
-  ordersLine.children[2].textContent = sum + ' $';
+  ordersLine.children[2].children[0].textContent = sum;
 }
 
 function showCheckboxCost() {
@@ -57,13 +65,13 @@ function showCheckboxCost() {
     accountingLine.style.display = 'none';
   } else {
     accountingLine.style.display = 'flex';
-    accountingLine.children[2].textContent = +firstCheckbox.value  + ' $';
+    accountingLine.children[2].children[0].textContent = +firstCheckbox.value;
   }
   if (!secondCheckbox.checked) {
     terminalLine.style.display = 'none';
   } else {
     terminalLine.style.display = 'flex';
-    terminalLine.children[2].textContent = +secondCheckbox.value + ' $';
+    terminalLine.children[2].children[0].textContent = +secondCheckbox.value;
   }
 }
 
@@ -71,10 +79,13 @@ function showCheckboxCost() {
     el.addEventListener('click', function() {
       packageLine.style.display = 'flex';
       packageLine.children[1].textContent = el.textContent;
-      packageLine.children[2].textContent = +el.value + ' $';
+      packageLine.children[2].children[0].textContent = +el.value;
   })
 })
 
+allInputs.forEach((el) => {
+  el.addEventListener('input', updateCost);
+})
 firstNumberInput.addEventListener('input', showProductCost);
 secondNumberInput.addEventListener('input', showOrderCost);
 firstCheckbox.addEventListener('change', showCheckboxCost);
@@ -85,58 +96,3 @@ formCheckbox.forEach((el) => {
 packageFullList.forEach((el) => {
   el.addEventListener('click', updateCost);
 })
-
-
-
-
-// function calculator() {
-//   let firstSum = 0;
-//   let secondSum = 0;
-//   let counter = 0;
-//
-//   firstNumberInput.addEventListener('input', function () {
-//     if (firstNumberInput.value <= 0) {
-//       productLine.style.display = 'none';
-//     } else {
-//       productLine.style.display = 'flex';
-//       productLine.children[1].textContent = firstNumberInput.value + ' * 5 $';
-//       firstSum = firstNumberInput.value * 5;
-//     }
-//     productLine.children[2].textContent = firstSum + ' $';
-//   })
-//
-//   secondNumberInput.addEventListener('input', function() {
-//     if (secondNumberInput.value <= 0) {
-//       ordersLine.style.display = 'none';
-//     } else {
-//       ordersLine.style.display = 'flex';
-//       ordersLine.children[1].textContent = secondNumberInput.value + ' * 2 $';
-//       secondSum = secondNumberInput.value * 2;
-//     }
-//     ordersLine.children[2].textContent = secondSum + ' $';
-//   })
-//
-//   firstCheckbox.addEventListener('change', function() {
-//     if (!firstCheckbox.checked) {
-//       accountingLine.style.display = 'none';
-//     } else {
-//       accountingLine.style.display = 'flex';
-//       accountingLine.children[2].textContent = firstCheckbox.value  + ' $';
-//     }
-//   })
-//
-//   secondCheckbox.addEventListener('change', function() {
-//     if (!secondCheckbox.checked) {
-//       terminalLine.style.display = 'none';
-//     } else {
-//       terminalLine.style.display = 'flex';
-//       terminalLine.children[2].textContent =secondCheckbox.value + ' $';
-//     }
-//   })
-//
-// }
-// calculator();
-
-
-
-
