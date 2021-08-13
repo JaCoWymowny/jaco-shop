40c3ae4a36a291a9bfc948814fd6ef0a844b0b1e
JaCoWymowny <jacekkombo@gmail.com>
2021-08-13 13:36:55 +0200

    fix icon

--- js/dropdownToggle.js
+++ js/dropdownToggle.js
@@ -4,6 +4,29 @@ const dropdownImage = document.querySelector('.dropdown_arrow');
 const list = document.querySelector('.dropdown');
 const listElements = document.querySelectorAll('.dropdown li');
 
+const btn = document.querySelector('.hamburger');
+const div = document.querySelector('.hamburger-list');
+const closeMenuIcon = document.querySelector('.fa-bars');
+const openMenuIcon = document.querySelector('.fa-times-circle');
+const title = document.querySelector('.page-logo');
+
+
+// Hamburger Menu
+btn.addEventListener('click', function() {
+  if(div.style.display === 'flex'){
+    div.style.display = 'none';
+    closeMenuIcon.classList.remove('hidden');
+    openMenuIcon.classList.add('hidden');
+    title.style.display = 'block';
+  }else {
+    div.style.display = 'flex';
+    closeMenuIcon.classList.add('hidden');
+    openMenuIcon.classList.remove('hidden');
+    title.style.display = 'none';
+  }
+})
+
+// dropdown Open/close style
 function toggleList() {
   if (list.style.display === 'none') {
     list.style.display = 'flex';
