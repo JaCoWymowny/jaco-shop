553a1c69e087acf1477d272ece1137e6a63f1ddd
JaCoWymowny <jacekkombo@gmail.com>
2021-08-13 11:37:12 +0200

    fix icon

--- js/indexjs.js
+++ js/indexjs.js
@@ -1,10 +1,17 @@
-let btn = document.querySelector('.hamburger');
-let div = document.querySelector('.hamburger-list');
+const btn = document.querySelector('.hamburger');
+const div = document.querySelector('.hamburger-list');
+const closeMenuIcon = document.querySelector('.fa-bars');
+const openMenuIcon = document.querySelector('.fa-times-circle');
 
-btn.addEventListener('click', () =>{
-    if(div.style.display === 'none'){
-        div.style.display = 'block';
-    }else {
+
+btn.addEventListener('click', function() {
+    if(div.style.display === 'block'){
         div.style.display = 'none';
+        closeMenuIcon.classList.remove('hidden');
+        openMenuIcon.classList.add('hidden');
+    }else {
+        div.style.display = 'block';
+        closeMenuIcon.classList.add('hidden');
+        openMenuIcon.classList.remove('hidden');
     }
 })
